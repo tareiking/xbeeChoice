52fb6830a0f66efaa7391724c76ce70a848e0ae6
Tarei King <tareiking@gmail.com>
2011-10-17 15:31:06 +1300

    param class changed.. unsure where unsure files are.

---
+++ src/Param.cpp
@@ -0,0 +1,44 @@
+//
+//  Param.cpp
+//  emptyExample
+//
+//  Created by Tarei on 17/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "Param.h"
+
+Param::Param(){
+    name = "param name";
+}
+
+Param::Param(int _type, string _name, string _description, string _command){
+//    type = _type;
+//    name = _name;
+//    description = _description;
+//    command = _command;
+  
+    cout << "new param added";
+    // needs PIN identifier
+}
+
+string Param::getName(){
+    cout << name;
+    return name;
+}
+
+int Param::getType(){
+    return type;
+}
+
+string Param::getDescription(){
+    return description;
+}
+
+string Param::getCommand(){
+    return command;
+}
+
+int Param::getPin(){
+    return parentPin;
+}
